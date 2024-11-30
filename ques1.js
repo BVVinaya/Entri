348980095fb663e7589bcb7a284c40a0195a1c75
BVVinaya <152289705+BVVinaya@users.noreply.github.com>
2024-11-30 18:33:55 +0530

    Add files via upload

---
+++ ques1.js
@@ -0,0 +1,8 @@
+function func(x,y){
+    if(x>y){
+console.log(`${x}is greater than ${y}`)
+    }
+    else {
+console.log(`${y}is greater than ${x}`)
+    }
+}func(10,30);
---
+++ quest3.js
@@ -0,0 +1,14 @@
+ let arr=[2,-4,6,-9,0,-5,10,-12];
+for(let i=0;i<=arr.length;i++){
+if(arr[i]>0){
+    console.log(`${arr[i]} is a positive number`);
+}
+else if(arr[i]<0){
+    console.log(`${arr[i]} is a negative number`);
+}
+else{
+    console.log(`${arr[i]} is equal to zero`);
+}
+}
+
+   
